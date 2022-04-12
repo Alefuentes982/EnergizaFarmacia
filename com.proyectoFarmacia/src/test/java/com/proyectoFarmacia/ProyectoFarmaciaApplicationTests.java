8b79dadf4748e4f0ac797bac9f5e8fcfab500145
ale <jano231982@gmail.com>
2022-04-12 18:21:54 -0400

    first commit

---
+++ com.proyectoFarmacia/src/test/java/com/proyectoFarmacia/ProyectoFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoFarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
